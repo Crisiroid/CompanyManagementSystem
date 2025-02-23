ed06d7c9a317bd5f43f089986867a0a0399d7a18
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-02-23 15:23:00 +0330

    Created Salary CRD

---
+++ CompanyManagement/Controllers/SalariesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyManagement.Data;
+using CompanyManagement.Entities;
+
+namespace CompanyManagement.Controllers
+{
+    public class SalariesController : Controller
+    {
+        private readonly CompanyDBContext _context;
+
+        public SalariesController(CompanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var companyDBContext = _context.Salaries.Include(s => s.Employee);
+            return View(await companyDBContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.Salaries
+                .Include(s => s.Employee)
+                .FirstOrDefaultAsync(m => m.SalaryId == id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            return View(salary);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SalaryId,EmployeeId,BaseSalary,Bonus,Deduction,PaymentDate")] Salary salary)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(salary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", salary.EmployeeId);
+            return View(salary);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.Salaries.FindAsync(id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", salary.EmployeeId);
+            return View(salary);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SalaryId,EmployeeId,BaseSalary,Bonus,Deduction,PaymentDate")] Salary salary)
+        {
+            if (id != salary.SalaryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salary);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalaryExists(salary.SalaryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", salary.EmployeeId);
+            return View(salary);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salary = await _context.Salaries
+                .Include(s => s.Employee)
+                .FirstOrDefaultAsync(m => m.SalaryId == id);
+            if (salary == null)
+            {
+                return NotFound();
+            }
+
+            return View(salary);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salary = await _context.Salaries.FindAsync(id);
+            if (salary != null)
+            {
+                _context.Salaries.Remove(salary);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalaryExists(int id)
+        {
+            return _context.Salaries.Any(e => e.SalaryId == id);
+        }
+    }
+}
