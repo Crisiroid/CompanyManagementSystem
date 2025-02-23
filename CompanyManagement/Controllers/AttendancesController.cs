1f7705e9dd3a7b947d5f88edc32507bb64afbd37
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-02-23 15:13:06 +0330

    Created Attendance CRD

---
+++ CompanyManagement/Controllers/AttendancesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyManagement.Data;
+using CompanyManagement.Entities;
+
+namespace CompanyManagement.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private readonly CompanyDBContext _context;
+
+        public AttendancesController(CompanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var companyDBContext = _context.AttendanceRecords.Include(a => a.Employee);
+            return View(await companyDBContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.AttendanceRecords
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email");
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AttendanceId,EmployeeId,AttendanceDate,CheckInTime,CheckOutTime,Status")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.AttendanceRecords.FindAsync(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AttendanceId,EmployeeId,AttendanceDate,CheckInTime,CheckOutTime,Status")] Attendance attendance)
+        {
+            if (id != attendance.AttendanceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(attendance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AttendanceExists(attendance.AttendanceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", attendance.EmployeeId);
+            return View(attendance);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await _context.AttendanceRecords
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.AttendanceId == id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return View(attendance);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var attendance = await _context.AttendanceRecords.FindAsync(id);
+            if (attendance != null)
+            {
+                _context.AttendanceRecords.Remove(attendance);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AttendanceExists(int id)
+        {
+            return _context.AttendanceRecords.Any(e => e.AttendanceId == id);
+        }
+    }
+}
