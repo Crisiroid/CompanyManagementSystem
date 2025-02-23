68d7a64d536a4370274f8a421ac1b2a64293b782
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-02-23 15:24:26 +0330

    Created EmployeeProejct CRD

---
+++ CompanyManagement/Controllers/EmployeeProjectsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CompanyManagement.Data;
+using CompanyManagement.Entities;
+
+namespace CompanyManagement.Controllers
+{
+    public class EmployeeProjectsController : Controller
+    {
+        private readonly CompanyDBContext _context;
+
+        public EmployeeProjectsController(CompanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var companyDBContext = _context.EmployeeProjects.Include(e => e.Employee).Include(e => e.Project);
+            return View(await companyDBContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeProject = await _context.EmployeeProjects
+                .Include(e => e.Employee)
+                .Include(e => e.Project)
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employeeProject == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeProject);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email");
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,ProjectId,Role")] EmployeeProject employeeProject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employeeProject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", employeeProject.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectName", employeeProject.ProjectId);
+            return View(employeeProject);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeProject = await _context.EmployeeProjects.FindAsync(id);
+            if (employeeProject == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", employeeProject.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectName", employeeProject.ProjectId);
+            return View(employeeProject);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,ProjectId,Role")] EmployeeProject employeeProject)
+        {
+            if (id != employeeProject.EmployeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employeeProject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeProjectExists(employeeProject.EmployeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Email", employeeProject.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectName", employeeProject.ProjectId);
+            return View(employeeProject);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employeeProject = await _context.EmployeeProjects
+                .Include(e => e.Employee)
+                .Include(e => e.Project)
+                .FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employeeProject == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeProject);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employeeProject = await _context.EmployeeProjects.FindAsync(id);
+            if (employeeProject != null)
+            {
+                _context.EmployeeProjects.Remove(employeeProject);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeProjectExists(int id)
+        {
+            return _context.EmployeeProjects.Any(e => e.EmployeeId == id);
+        }
+    }
+}
